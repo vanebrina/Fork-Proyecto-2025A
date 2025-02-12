35f396d4707c8ccf18592322552e6b44e3bb6a20
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-12 08:01:01 -0500

    Critic logs

--- src/models/base/sia.py
+++ src/models/base/sia.py
@@ -82,18 +82,18 @@ class SIA(ABC):
 
         # Formación de datos con logs opcionales de ejemplificación
         completo = System(tpm, estado_inicial)
-        # self.sia_logger.critic("Original:")
+        self.sia_logger.critic("Original creado.")
         # self.sia_logger.info(completo)
         # self.sia_logger.critic("Original:")
         # self.sia_logger.info(completo)
 
         candidato = completo.condicionar(dims_condicionadas)
-        # self.sia_logger.warn("Candidato:")
+        self.sia_logger.warn("Candidato creado.")
         # self.sia_logger.info(f"{dims_condicionadas}")
         # self.sia_logger.debug(candidato)
 
         subsistema = candidato.substraer(dims_alcance, dims_mecanismo)
-        # self.sia_logger.warn("Subsys:")
+        self.sia_logger.warn("Subsistema creado.")
         # self.sia_logger.debug(f"{dims_alcance, dims_mecanismo=}")
         # self.sia_logger.error(subsistema)
 
