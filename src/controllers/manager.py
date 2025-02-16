d71cc1981b236bcff77f8f1d94cb33d79eb43987
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-16 09:49:35 -0500

    Better Manager class doc

--- src/controllers/manager.py
+++ src/controllers/manager.py
@@ -16,17 +16,18 @@ from src.constants.base import (
 
 @dataclass
 class Manager:
-    """El manejador es el encargado de en funcioón al tamaño del estado inicial y la página asociada, el traer el fichero de formato CSV con las TPM's almacenadas en `.samples/` para hacer una rápida depuración de los datos para la creación de sistemas.
+    """
+    El gestor es el encargado de en función al tamaño del estado inicial y la página asociada traer el fichero de formato CSV con las TPM's almacenadas en `.samples/` para hacer una rápida depuración de los datos para la creación de sistemas.
 
     Args:
     ----
         - `estado_inicial` (str): Dado se manejan sistemas binarios es un número base dos de tamaño asociado a la red que se quiera cargar.
         - `pagina` (str): En la ruta de samples se tiene un literal asociado al tamaño de las redes por si se necesita añadir varias de un mismo tamaño.
-        ruta_base (Path): Ruta donde se encuentran las muestras de TPMs en representación estado-nodo-on.
+        ruta_base (Path): Ruta donde se encuentran las muestras de TPMs en representación estado-nodo-on (TPM estado-nodo simplificada).
 
     Returns:
     -------
-        None: Así mismo se encarga de asociar el directorio donde se mostrarán análisis sobre las ejecuciones, donde el programador haga uso del módulo de logging y profilling.
+        Manager: Así mismo se encarga de asociar el directorio donde se mostrarán análisis de las ejecuciones, donde sea el programador haga uso del módulo de logging y profilling.
     """
 
     estado_inicial: str
