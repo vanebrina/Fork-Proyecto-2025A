2176721b48f7431ba100e693c8200376483d1390
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-16 11:32:21 -0500

    Merge with main

--- src/main.py
+++ src/main.py
@@ -5,19 +5,11 @@ from src.controllers.strategies.force import BruteForce
 
 def iniciar():
     """Punto de entrada principal"""
-<<<<<<< HEAD
-                   # ABCD #
-    estado_inicio = "1000"
-    condiciones =   "1111"
-    alcance =       "1111"
-    mecanismo =     "1111"
-=======
                     # ABCD #
     estado_inicial = "1000"
     condiciones =    "1110"
     alcance =        "1110"
     mecanismo =      "1110"
->>>>>>> 32c873b3fdb7bec96c89605e78c9201e79623712
 
     gestor_sistema = Manager(estado_inicial)
 
