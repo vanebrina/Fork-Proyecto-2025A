4e19772c36141eea6e52bbd013cbd5ba67b4128f
OH <over.castrillon22396@ucaldas.edu.co>
2025-03-01 03:17:04 -0500

    Main junction

--- exec.py
+++ exec.py
@@ -1,12 +1,12 @@
-from src.middlewares.profile import profiler_manager
 from src.models.base.application import aplicacion
 
-# from src.main import iniciar
-from src.alt import iniciar
+from src.main import iniciar
+# from src.alt import iniciar
 
 
 def main():
     """Inicializar el aplicativo."""
+
     aplicacion.profiler_habilitado = True
     # aplicacion.pagina_sample_network = "B"
 
