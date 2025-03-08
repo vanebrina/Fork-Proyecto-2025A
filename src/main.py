e9146a24f52009cd27271409e361d399dce80f5b
OH <over.castrillon22396@ucaldas.edu.co>
2025-03-08 00:16:56 -0500

    Spacy

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from src.controllers.strategies.phi import Phi
+from src.controllers.strategies.q_nodes import QNodes
 from src.controllers.manager import Manager
 
 from src.controllers.strategies.force import BruteForce
@@ -5,16 +7,16 @@ from src.controllers.strategies.force import BruteForce
 
 def iniciar():
     """Punto de entrada principal"""
-                    # ABCD #
-    estado_inicial = "1000"
-    condiciones =    "1110"
-    alcance =        "1110"
-    mecanismo =      "1110"
+                    # ABCDEF #
+    estado_inicial = "100000"
+    condiciones =    "111111"
+    alcance =        "111111"
+    mecanismo =      "111111"
 
     gestor_sistema = Manager(estado_inicial)
-
+    
     ### Ejemplo de solución mediante módulo de fuerza bruta ###
-    analizador_fb = BruteForce(gestor_sistema)
+    analizador_fb = QNodes(gestor_sistema)
     sia_uno = analizador_fb.aplicar_estrategia(
         condiciones,
         alcance,
