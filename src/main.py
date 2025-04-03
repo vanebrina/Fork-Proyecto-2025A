cfed4782ffcff47eec504f26250fbb95622faa1c
vanebrina <seider.sanin22135@ucaldas.edu.co>
2025-04-03 11:56:06 -0500

    Refactor main.py to use Phi strategy and rename initial state variable

--- src/main.py
+++ src/main.py
@@ -1,23 +1,20 @@
 from src.controllers.manager import Manager
 
 from src.controllers.strategies.q_nodes import QNodes
+from src.controllers.strategies.phi import Phi
 
 
 def iniciar():
     """Punto de entrada principal"""
                     # ABCD #
-    estado_inicial = "1000"
+    estado_inicio = "1000"
     condiciones =    "1110"
     alcance =        "1110"
     mecanismo =      "1110"
 
-    gestor_sistema = Manager(estado_inicial)
+    gestor_sistema = Manager(estado_inicial=estado_inicio)
 
     ### Ejemplo de solución mediante módulo de fuerza bruta ###
-    analizador_fb = QNodes(gestor_sistema)
-    sia_uno = analizador_fb.aplicar_estrategia(
-        condiciones,
-        alcance,
-        mecanismo,
-    )
+    analizador_fb = Phi(gestor_sistema)
+    sia_uno = analizador_fb.aplicar_estrategia(condiciones, alcance, mecanismo)
     print(sia_uno)
